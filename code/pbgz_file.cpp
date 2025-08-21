6eb631a4c45e62a3f81449dbc159f21088713414
laverrer <la_verrer@126.com>
2025-08-21 17:02:34 +0800

    Implement pbgz format file reader

--- code/pbgz_file.cpp
+++ code/pbgz_file.cpp
@@ -42,79 +42,95 @@ std::string PbgzFileHeader::getVersionStr() {
 
 int32_t PbgzFileHeader::serialize(uint8_t* buffer, uint32_t bufferLength, uint32_t& dataLength) {
     if (blockType != FILE_HEADER) {
-        
+        LOG_STDOUT(LOG_ERROR, "Invalid block type for serialization: %d", blockType);
         return -1;
     }
-    if (bufferLength < PBGZ_FILE_MAGIC.length() + sizeof(version)) {
+    if (bufferLength < PBGZ_FILE_MAGIC_LENGTH + sizeof(version)) {
         return -1; // Buffer too small
     }
 
     // Serialize block type, 4 byte without '\0'
-    memcpy(buffer, &PBGZ_FILE_MAGIC, PBGZ_FILE_MAGIC.length());
+    memcpy(buffer, &PBGZ_FILE_MAGIC, PBGZ_FILE_MAGIC_LENGTH);
     // Serialize version
-    memcpy(buffer + PBGZ_FILE_MAGIC.length(), version, sizeof(version));
+    memcpy(buffer + PBGZ_FILE_MAGIC_LENGTH, version, sizeof(version));
 
-    dataLength = PBGZ_FILE_MAGIC.length() + sizeof(version);
+    dataLength = PBGZ_FILE_MAGIC_LENGTH + sizeof(version);
     return 0; 
 }
 
 int32_t PbgzFileHeader::unserialize(uint8_t* buffer, uint32_t bufferLength) {
-    if (bufferLength < PBGZ_FILE_MAGIC.length() + sizeof(version)) {
+    if (bufferLength < PBGZ_FILE_MAGIC_LENGTH + sizeof(version)) {
         return -1; // Buffer too small
     }
 
     // Unserialize block type
-    memcpy(&blockType, buffer, PBGZ_FILE_MAGIC.length());
+    if (strncmp(reinterpret_cast<const char*>(buffer), PBGZ_FILE_MAGIC.c_str(), PBGZ_FILE_MAGIC_LENGTH) != 0) {
+        LOG_STDOUT(LOG_ERROR, "Invalid magic value for PBGZ file header.");
+        return -1; // Invalid magic
+    }
+    blockType = FILE_HEADER;
+
     // Unserialize version
-    memcpy(version, buffer + PBGZ_FILE_MAGIC.length(), sizeof(version));
+    memcpy(version, buffer + PBGZ_FILE_MAGIC_LENGTH, sizeof(version));
 
     return 0; 
 }
 
 int32_t PbgzFileMeta::serialize(uint8_t* buffer, uint32_t bufferLength, uint32_t& dataLength) {
+    if (blockType != FILE_META) {
+        LOG_STDOUT(LOG_ERROR, "Invalid block type for serialization: %d", blockType);
+        return -1;
+    }   
+
     Json::FastWriter writer;
     std::string jsonString = writer.write(metaData);
     metaLength = jsonString.size();
-    if (bufferLength < sizeof(PBGZ_FILE_META_MAGIC) + metaLength + sizeof(metaChecksum)) {
+    if (bufferLength < PBGZ_FILE_META_MAGIC_LENGTH + PBGZ_FILE_META_SIZE_LENGTH + metaLength + PBGZ_FILE_META_CHECKSUM_LENGTH) {
         return -1; // Buffer too small
     }
 
     uint32_t dataOffset = 0;
     // Serialize block type
-    memcpy(buffer, &blockType, sizeof(PBGZ_FILE_META_MAGIC));
-    dataOffset += sizeof(PBGZ_FILE_META_MAGIC);
+    memcpy(buffer, &PBGZ_FILE_META_MAGIC, PBGZ_FILE_META_MAGIC_LENGTH);
+    dataOffset += PBGZ_FILE_META_MAGIC_LENGTH;
 
     // Serialize meta data length
-    memcpy(buffer + dataOffset, &metaLength, sizeof(metaLength));
-    dataOffset += sizeof(metaLength);
+    memcpy(buffer + dataOffset, &metaLength, PBGZ_FILE_META_SIZE_LENGTH);
+    dataOffset += PBGZ_FILE_META_SIZE_LENGTH;
 
     // Serialize meta data
+    // 暂时没有加密，待实现
+    /// TODO: Implement meta data encryption if needed
     memcpy(buffer + dataOffset, jsonString.c_str(), metaLength);
     dataOffset += metaLength;
 
     // Serialize checksum
-    memcpy(buffer + dataOffset, metaChecksum, sizeof(metaChecksum));
+    memcpy(buffer + dataOffset, metaChecksum, PBGZ_FILE_META_CHECKSUM_LENGTH);
 
-    dataLength = sizeof(PBGZ_FILE_META_MAGIC) + jsonString.size() + sizeof(metaChecksum);
+    dataLength = dataOffset + PBGZ_FILE_META_CHECKSUM_LENGTH;
     return 0; 
 }
 
 int32_t PbgzFileMeta::unserialize(uint8_t* buffer, uint32_t bufferLength) {
-    if (bufferLength < sizeof(PBGZ_FILE_META_MAGIC) + sizeof(metaChecksum)) {
+    if (bufferLength < PBGZ_FILE_META_MAGIC_LENGTH + PBGZ_FILE_META_SIZE_LENGTH + sizeof(metaChecksum)) {
         return -1; // Buffer too small
     }
 
     uint dataOffset = 0;
     // Unserialize block type
-    memcpy(&blockType, buffer, sizeof(PBGZ_FILE_META_MAGIC));
-    dataOffset += sizeof(PBGZ_FILE_META_MAGIC);
+    if (memcmp(buffer, &PBGZ_FILE_META_MAGIC, PBGZ_FILE_META_MAGIC_LENGTH) != 0) {
+        LOG_STDOUT(LOG_ERROR, "Invalid magic value for PBGZ file meta.");
+        return -1; // Invalid magic
+    }
+    blockType = FILE_META;
+    dataOffset += PBGZ_FILE_META_MAGIC_LENGTH;
 
     // Unserialize meta data length
-    if (bufferLength < dataOffset + sizeof(metaLength)) {
+    if (bufferLength < dataOffset + PBGZ_FILE_META_SIZE_LENGTH) {
         return -   1 ; // Buffer too small for metaLength   
     }
     memcpy(&metaLength, buffer + dataOffset, sizeof(metaLength));
-    dataOffset += sizeof(metaLength);   
+    dataOffset += PBGZ_FILE_META_SIZE_LENGTH;   
 
     // Unserialize meta data
     std::string jsonString(reinterpret_cast<char*>(buffer + dataOffset), metaLength);
@@ -122,128 +138,142 @@ int32_t PbgzFileMeta::unserialize(uint8_t* buffer, uint32_t bufferLength) {
     std::istringstream jsonStream(jsonString);
     std::string errs;
     if (!Json::parseFromStream(readerBuilder, jsonStream, &metaData, &errs)) {
-        std::cerr << "Failed to parse JSON: " << errs << std::endl;
+        LOG_STDOUT(LOG_ERROR, "Failed to parse JSON: %s", errs);
         return -1; // JSON parse error
     }
     dataOffset += metaLength;
 
     // Unserialize checksum
-    memcpy(metaChecksum, buffer + dataOffset, sizeof(metaChecksum));
+    memcpy(metaChecksum, buffer + dataOffset, PBGZ_FILE_META_CHECKSUM_LENGTH);
 
     return 0; 
 }
 
 
 int32_t PbgzDataBlock::serialize(uint8_t* buffer, uint32_t bufferLength, uint32_t& dataLength) {
+    if (blockType != FILE_DATA) {
+        LOG_STDOUT(LOG_ERROR, "Invalid block type for serialization: %d", blockType);
+        return -1;
+    }
+
     Json::FastWriter writer;
-    std::string jsonString = writer.write(metaData);
-    metaLength = jsonString.size();
-    if (bufferLength < sizeof(PBGZ_DATA_BLOCK_MAGIC) + sizeof(metaLength) + metaLength + sizeof(dataLength) + dataLength + sizeof(dataBlockChecksum) + sizeof(originDataChecksum)) {
+    std::string jsonString = writer.write(dataMetaInfo);
+    dataMetaLength = jsonString.size();
+    if (bufferLength < PBGZ_DATA_BLOCK_MAGIC_LENGTH +  PBGZ_DATA_BLOCK_META_SIZE_LENGTH + dataMetaLength + 
+        PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH + blockDataLength + PBGZ_DATA_BLOCK_CHECKSUM_LENGTH + PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Buffer too small for serialization");
         return -1; // Buffer too small
     }
 
     uint32_t dataOffset = 0;
     // Serialize block type
-    memcpy(buffer, &blockType, sizeof(PBGZ_DATA_BLOCK_MAGIC));
-    dataOffset += sizeof(PBGZ_DATA_BLOCK_MAGIC);
+    memcpy(buffer, &PBGZ_DATA_BLOCK_MAGIC, PBGZ_DATA_BLOCK_MAGIC_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_MAGIC_LENGTH;
 
     // Serialize meta length
-    memcpy(buffer + dataOffset, &metaLength, sizeof(metaLength));
-    dataOffset += sizeof(metaLength);
+    memcpy(buffer + dataOffset, &dataMetaLength, PBGZ_DATA_BLOCK_META_SIZE_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_META_SIZE_LENGTH;
 
     // Serialize meta data
-    memcpy(buffer + dataOffset, jsonString.c_str(), metaLength);
-    dataOffset += metaLength;
+    memcpy(buffer + dataOffset, jsonString.c_str(), dataMetaLength);
+    dataOffset += dataMetaLength;
 
     // Serialize data length
     memcpy(buffer + dataOffset, &dataLength, sizeof(dataLength));
     dataOffset += sizeof(dataLength);
 
     // Serialize block data
-    if (pData && dataLength > 0) {
-        memcpy(buffer + dataOffset, pData, dataLength);
+    if (pBlockData && dataLength > 0) {
+        memcpy(buffer + dataOffset, pBlockData, dataLength);
         dataOffset += dataLength;
     }
 
     // Serialize checksums
-    memcpy(buffer + dataOffset, dataBlockChecksum, sizeof(dataBlockChecksum));
-    dataOffset += sizeof(dataBlockChecksum);
+    memcpy(buffer + dataOffset, dataBlockChecksum, PBGZ_DATA_BLOCK_CHECKSUM_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_CHECKSUM_LENGTH;
     
-    memcpy(buffer + dataOffset, originDataChecksum, sizeof(originDataChecksum));
-    dataLength = sizeof(PBGZ_DATA_BLOCK_MAGIC) + sizeof(metaLength) + metaLength + sizeof(dataLength) + dataLength + sizeof(dataBlockChecksum) + sizeof(originDataChecksum);
+    memcpy(buffer + dataOffset, originDataChecksum, PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH );
+    dataLength = dataOffset + PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH;
     
     return 0; 
 }
 
 
 int32_t PbgzDataBlock::unserialize(uint8_t* buffer, uint32_t bufferLength) {
-    if (bufferLength < sizeof(PBGZ_DATA_BLOCK_MAGIC) + sizeof(metaLength) + sizeof(dataLength) + sizeof(dataBlockChecksum) + sizeof(originDataChecksum)) {
+    if (bufferLength < PBGZ_DATA_BLOCK_MAGIC_LENGTH + PBGZ_DATA_BLOCK_META_SIZE_LENGTH + PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH
+         + PBGZ_DATA_BLOCK_CHECKSUM_LENGTH + PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH) {
         return -1; // Buffer too small
     }
 
     uint32_t dataOffset = 0;
     // Unserialize block type
-    memcpy(&blockType, buffer, sizeof(PBGZ_DATA_BLOCK_MAGIC));
-    dataOffset += sizeof(PBGZ_DATA_BLOCK_MAGIC);
+    if (memcmp(buffer, &PBGZ_DATA_BLOCK_MAGIC, PBGZ_DATA_BLOCK_MAGIC_LENGTH) != 0) {
+        LOG_STDOUT(LOG_ERROR, "Invalid magic value for PBGZ data block.");
+        return -1; // Invalid magic
+    }
+
+    blockType = FILE_DATA;
+    dataOffset += PBGZ_DATA_BLOCK_MAGIC_LENGTH;
 
     // Unserialize meta length
-    memcpy(&metaLength, buffer + dataOffset, sizeof(metaLength));
-    dataOffset += sizeof(metaLength);
+    memcpy(&dataMetaLength, buffer + dataOffset, PBGZ_DATA_BLOCK_META_SIZE_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_META_SIZE_LENGTH;
 
     // Unserialize meta data
-    if (bufferLength < dataOffset + metaLength) {
+    if (bufferLength < dataOffset + dataMetaLength) {
         return -1; // Buffer too small for meta data
     }
-    std::string jsonString(reinterpret_cast<char*>(buffer + dataOffset), metaLength);
+    std::string jsonString(reinterpret_cast<char*>(buffer + dataOffset), dataMetaLength);
     Json::CharReaderBuilder readerBuilder;
     std::istringstream jsonStream(jsonString);
     std::string errs;
-    if (!Json::parseFromStream(readerBuilder, jsonStream, &metaData, &errs)) {
+    if (!Json::parseFromStream(readerBuilder, jsonStream, &dataMetaInfo, &errs)) {
         LOG_STDOUT(LOG_FATAL, "Failed to parse JSON: %s", errs.c_str());
         return -1; // JSON parse error
     }
-    dataOffset += metaLength;
+    dataOffset += dataMetaLength;
 
     // Unserialize data length
-    memcpy(&dataLength, buffer + dataOffset, sizeof(dataLength));
-    dataOffset += sizeof(dataLength);
+    memcpy(&blockDataLength, buffer + dataOffset, PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH;
 
-    if (bufferLength < dataOffset + dataLength ) {
+    if (bufferLength < dataOffset + blockDataLength ) {
         return -1; // Buffer too small for data
     }
 
     // Unserialize block data
-    if (dataLength > 0) {
-        pData = static_cast<uint8_t*>(malloc(dataLength));
-        if (!pData) {
+    if (blockDataLength > 0) {
+        pBlockData = static_cast<uint8_t*>(malloc(blockDataLength));
+        if (!pBlockData) {
+            LOG_STDOUT(LOG_FATAL, "Failed to allocate memory for block data.");
             return -1; // Memory allocation failed
         }
-        memcpy(pData, buffer + dataOffset, dataLength);
-        dataOffset += dataLength;
+        memcpy(pBlockData, buffer + dataOffset, blockDataLength);
+        dataOffset += blockDataLength;
     }
 
     // Unserialize checksums
-    memcpy(dataBlockChecksum, buffer + dataOffset, sizeof(dataBlockChecksum));
-    dataOffset += sizeof(dataBlockChecksum);
+    memcpy(dataBlockChecksum, buffer + dataOffset, PBGZ_DATA_BLOCK_CHECKSUM_LENGTH);
+    dataOffset += PBGZ_DATA_BLOCK_CHECKSUM_LENGTH;
     
-    memcpy(originDataChecksum, buffer + dataOffset, sizeof(originDataChecksum));
+    memcpy(originDataChecksum, buffer + dataOffset, PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH);
 
     return 0; 
 }
 
 int32_t PbgzDataBlock::setBlockData(uint8_t* data, uint32_t length) {
-    if (pData) {
-        free(pData);
-        pData = nullptr;
+    if (pBlockData) {
+        free(pBlockData);
+        pBlockData = nullptr;
     }
     
-    pData = static_cast<uint8_t*>(malloc(length));
-    if (!pData) {
+    pBlockData = static_cast<uint8_t*>(malloc(length));
+    if (!pBlockData) {
         std::cerr << "Failed to allocate memory for block data." << std::endl;
         return -1;
     }
 
-    memcpy(pData, data, length);
+    memcpy(pBlockData, data, length);
     return 0;
 }
 
--- code/pbgz_file_handler.cpp
+++ code/pbgz_file_handler.cpp
@@ -24,40 +24,258 @@
 #include "pbgz_file_handler.h"
 #include "log/logger.h"
 
+// 2MB read buffer size
+const uint32_t PBGZ_FILE_READ_BUFFER_LENGTH = 2 * 1024 * 1024; 
+
+uint8_t* PbgzFileReader::getFileReadBuffer(){
+    thread_local static uint8_t* pReadBuffer = nullptr;
+    if (!pReadBuffer) {
+        pReadBuffer = new uint8_t[PBGZ_FILE_READ_BUFFER_LENGTH];
+        if (!pReadBuffer) {
+            LOG_STDOUT(LOG_ERROR, "Failed to allocate memory for read buffer.");
+            return nullptr; // Memory allocation error
+        }
+    }
+    return pReadBuffer;
+}
+
 int32_t PbgzFileReader::open() {
     // Implement the read logic for PBGZ file format
     // This is a placeholder implementation
     LOG_STDOUT(LOG_INFO, "Reading PBGZ file...");
-    FILE* pFile = fopen(fileName.c_str(), "rb");
+    pFile = fopen(fileName.c_str(), "rb");
     if (!pFile) {
         LOG_STDOUT(LOG_ERROR, "Failed to open file: %s", fileName.c_str());
         return -1; // File open error
     }
 
-    // Read the file header
-    char buffer[7];
-    size_t readLen = fread(buffer, sizeof(buffer), 1, pFile);
-    if (readLen !=  sizeof(buffer)) {
-        LOG_STDOUT(LOG_ERROR, "Failed to read file header from: %s", fileName.c_str());
+    if (0 != initFileHeadAndMeta()) {
         fclose(pFile);
-        return -1; // File read error
+        return -1; // Initialization error
     }
 
-    if (strncmp(buffer, PBGZ_FILE_MAGIC.c_str(), 4) != 0) {
-        // 安照16进制打印，自行解析
-        LOG_STDOUT(LOG_ERROR, "%s is not a valid pbgz file, magic no is %X", fileName.c_str(), (uint32_t)buffer);
-        fclose(pFile);
-        return -1; // Invalid magic
+    return 0;
+}
+
+int32_t PbgzFileReader::initFileHeadAndMeta(bool isCheckMagic) {
+    // Read the file header
+    uint8_t* pReadBuffer = getFileReadBuffer();
+    if (!pReadBuffer) {
+        LOG_STDOUT(LOG_ERROR, "Failed to get read buffer.");   
+        return -1; 
     }
+
+    // Read the magic value, 4 byte
+    size_t readLen = 0;
+    // If isCheckMagic is true, we will not read the magic value again
+    if (isCheckMagic) {
+        memcpy(pReadBuffer, PBGZ_FILE_MAGIC.c_str(), PBGZ_FILE_MAGIC_LENGTH);
+    } else {
+        readLen = fread(pReadBuffer, PBGZ_FILE_MAGIC_LENGTH, 1, pFile);
+        if (readLen !=  PBGZ_FILE_MAGIC_LENGTH) {
+            LOG_STDOUT(LOG_ERROR, "Failed to read file header from: %s", fileName.c_str());
+            return -1; // File read error
+        }
+
+        if (memcmp(pReadBuffer, PBGZ_FILE_MAGIC.c_str(), PBGZ_FILE_MAGIC_LENGTH) != 0) {
+            // 安照16进制打印，自行解析
+            LOG_STDOUT(LOG_ERROR, "%s is not a valid pbgz file, magic no is %X", fileName.c_str(), (uint32_t)pReadBuffer);
+            return -1; // Invalid magic
+        }
+    }
+
+    // Read the version number, 3 byte
+    readLen = fread(pReadBuffer + PBGZ_FILE_MAGIC_LENGTH, 1, PBGZ_FILE_VERSION_LENGTH, pFile);
+    if (readLen != PBGZ_FILE_VERSION_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read file version from: %s", fileName.c_str());
+        return -1; // File read error
+    }   
 
     LOG_STDOUT(LOG_INFO, "PBGZ file opened successfully: %s", fileName.c_str());
 
     PbgzFileHeader header;
-    header.unserialize(reinterpret_cast<uint8_t*>(buffer), sizeof(buffer));
+    header.unserialize(reinterpret_cast<uint8_t*>(pReadBuffer), PBGZ_FILE_MAGIC_LENGTH + PBGZ_FILE_VERSION_LENGTH);
     fileHeaderMap[currentFileIndex++] = header;
 
     // Read file meta information
-    PbgzFileMeta fileMeta;
+    // read file meta magic
+    readLen = fread(pReadBuffer, PBGZ_FILE_META_MAGIC_LENGTH, 1, pFile);
+    if (readLen != PBGZ_FILE_META_MAGIC_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "%s is not a valid pbgz file.", fileName.c_str());
+        return -1;
+    }
 
-    return 0; // Return success status
+    if (0 != memcmp(pReadBuffer, &PBGZ_FILE_META_MAGIC, PBGZ_FILE_META_MAGIC_LENGTH)) {
+        LOG_STDOUT(LOG_ERROR, "%s is not a valid pbgz file, file meta magic no is %X", fileName.c_str(), (uint32_t)pReadBuffer);
+        return -1;
+    }
+
+    // read file meta length
+    readLen = fread(pReadBuffer + PBGZ_FILE_META_MAGIC_LENGTH, 1, PBGZ_FILE_META_SIZE_LENGTH, pFile);
+    if (readLen != PBGZ_FILE_META_SIZE_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read file meta length from: %s", fileName.c_str());
+        return -1; // File read error       
+    }
+    const uint32_t metaLength = (uint32_t)(pReadBuffer + PBGZ_FILE_META_MAGIC_LENGTH);
+
+    // read the rest of the meta data and checksum
+    readLen = fread(pReadBuffer + PBGZ_FILE_META_MAGIC_LENGTH + PBGZ_FILE_META_SIZE_LENGTH, 1, metaLength + PBGZ_FILE_META_CHECKSUM_LENGTH, pFile);
+    if (readLen != metaLength + PBGZ_FILE_META_CHECKSUM_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read file meta data from: %s", fileName.c_str());
+        return -1; // File read error   
+    }
+
+    PbgzFileMeta fileMeta;
+    // file meta 的总长度：maigic头（4）+ metaLength（4）+ metaData + checksum（8）
+    uint32_t fileMetaDataLength = PBGZ_FILE_META_MAGIC_LENGTH + PBGZ_FILE_META_SIZE_LENGTH + metaLength + PBGZ_FILE_META_CHECKSUM_LENGTH;
+    int ret = fileMeta.unserialize(pReadBuffer, fileMetaDataLength);
+    if (ret != 0) {
+        LOG_STDOUT(LOG_ERROR, "Failed to unserialize file meta data from: %s", fileName.c_str());
+        return -1; // Unserialization error
+    }
+
+    // 保持和header相同的序列号
+    fileMetaMap[currentFileIndex] = fileMeta;
+    return 0;
+}       
+
+void PbgzFileReader::close() {
+    if (pFile) {
+        fclose(pFile);
+        pFile = nullptr;
+    }
+
+    return;
 }
+
+PbgzFileReader::~PbgzFileReader() {
+    if (pFile) {
+        fclose(pFile);
+    }
+}
+
+const std::map<int32_t, PbgzFileHeader>& PbgzFileReader::getFileHeader() const {
+    return fileHeaderMap;
+}
+
+const std::map<int32_t, PbgzFileMeta>& PbgzFileReader::getFileMeta() const {
+    return fileMetaMap;
+}
+
+int32_t PbgzFileReader::readDataBlock(PbgzDataBlock& dataBlock) {
+    LOG_STDOUT(LOG_INFO, "Reading PBGZ data block...");
+    if (!pFile) {
+        LOG_STDOUT(LOG_ERROR, "File is not open.");
+        return -1; // File not open
+    }   
+
+    uint8_t* pReadBuffer = getFileReadBuffer();
+    if (!pReadBuffer) {
+        LOG_STDOUT(LOG_ERROR, "Failed to get read buffer.");        
+        return -1; // Memory allocation error
+    }
+
+    // Read the data block magic value, 4byte
+    size_t readLen = fread(pReadBuffer, PBGZ_DATA_BLOCK_MAGIC_LENGTH, 1, pFile);
+    if (readLen != PBGZ_DATA_BLOCK_MAGIC_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block header.");
+        return -1; // File read error
+    }
+
+    if (memcmp(pReadBuffer, &PBGZ_DATA_BLOCK_MAGIC, PBGZ_DATA_BLOCK_MAGIC_LENGTH) != 0) {
+        LOG_STDOUT(LOG_ERROR, "Invalid magic value for PBGZ data block. %X", (uint32_t)pReadBuffer);
+        // It maybe a new file, try to parse the file header and meta information
+         if (memcmp(pReadBuffer, &PBGZ_FILE_MAGIC, PBGZ_FILE_MAGIC_LENGTH) == 0) {
+            LOG_STDOUT(LOG_INFO, "Detected a new PBGZ file format, reinitializing file header and meta.");
+            // 由于已经读取了文件的Maigc值,不需要在读取了
+            if (0 != initFileHeadAndMeta(true)) {
+                LOG_STDOUT(LOG_ERROR, "Failed to initialize file header and meta.");
+                return -1; // Initialization error
+            }
+
+            // continue to read the data block from new file
+            return readDataBlock(dataBlock); 
+        }
+        return -1; // Invalid magic
+    }
+
+    // Read the meta length, 4byte
+    readLen = fread(pReadBuffer + PBGZ_DATA_BLOCK_MAGIC_LENGTH, 1, PBGZ_DATA_BLOCK_META_SIZE_LENGTH, pFile);
+    if (readLen != PBGZ_DATA_BLOCK_META_SIZE_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block meta size.");
+        return -1; // File read error           
+    }
+
+    uint32_t metaLength = (uint32_t)(pReadBuffer + PBGZ_DATA_BLOCK_MAGIC_LENGTH);
+    if (metaLength == 0) {
+        LOG_STDOUT(LOG_ERROR, "Meta length is zero, invalid data block.");
+        return -1; // Invalid meta length       
+    }
+
+    // Read the meta data
+    readLen = fread(pReadBuffer + PBGZ_DATA_BLOCK_MAGIC_LENGTH + PBGZ_DATA_BLOCK_META_SIZE_LENGTH, 1, metaLength, pFile);
+    if (readLen != metaLength) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block meta data."); 
+        return -1; // File read error   
+    }
+
+    // Read the data length, 4byte
+    uint32_t offset = PBGZ_DATA_BLOCK_MAGIC_LENGTH + PBGZ_DATA_BLOCK_META_SIZE_LENGTH + metaLength;
+    readLen = fread(pReadBuffer + offset, 1, PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH, pFile);
+    if (readLen != PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block data length.");
+        return -1; // File read error
+    }   
+    
+    uint32_t dataLength = (uint32_t)(pReadBuffer + offset);
+    if (dataLength == 0) {
+        LOG_STDOUT(LOG_ERROR, "Data length is zero, invalid data block.");  
+        return -1; // Invalid data length
+    }
+
+    // Read the block data
+    offset += PBGZ_DATA_BLOCK_DATA_SIZE_LENGTH;
+    readLen = fread(pReadBuffer + offset, 1, dataLength, pFile);
+    if (readLen != dataLength) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block data.");
+        return -1; // File read error
+    }
+
+    // Read the data block checksum, 8byte
+    offset += dataLength;
+    readLen = fread(pReadBuffer + offset, 1, PBGZ_DATA_BLOCK_CHECKSUM_LENGTH, pFile);
+    if (readLen != PBGZ_DATA_BLOCK_CHECKSUM_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block checksum.");
+        return -1; // File read error
+    }
+
+    // Read the original data checksum, 8byte
+    offset += PBGZ_DATA_BLOCK_CHECKSUM_LENGTH;
+    readLen = fread(pReadBuffer + offset, 1, PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH, pFile);
+    if (readLen != PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH) {
+        LOG_STDOUT(LOG_ERROR, "Failed to read PBGZ data block original checksum.");     
+        return -1; // File read error
+    }
+
+    // Unserialize the data block
+    int ret = dataBlock.unserialize(pReadBuffer, offset + PBGZ_DATA_BLOCK_ORIGIN_CHECKSUM_LENGTH);
+    if (ret != 0) {
+        LOG_STDOUT(LOG_ERROR, "Failed to unserialize PBGZ data block.");
+        return -1; // Unserialization error 
+    }
+        
+    return 0;
+}   
+
+
+uint8_t* PbgzFileWriter::getFileWriteBuffer() {
+    thread_local static uint8_t* pWriteBuffer = nullptr;
+    if (!pWriteBuffer) {
+        pWriteBuffer = new uint8_t[PBGZ_FILE_READ_BUFFER_LENGTH];
+        if (!pWriteBuffer) {
+            LOG_STDOUT(LOG_ERROR, "Failed to allocate memory for write buffer.");
+            return nullptr; // Memory allocation error
+        }
+    }
+    return pWriteBuffer;
+}; 
