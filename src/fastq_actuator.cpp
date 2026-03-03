e370db80b9746afab0a5bbf09fd71294e6c7b47c
laverrer <laverrer@126.com>
2026-03-03 10:05:19 +0800

    Fix the crash issue in the compressed fastq gen3 program

--- src/fastq_actuator.cpp
+++ src/fastq_actuator.cpp
@@ -766,7 +766,7 @@ int32_t FastqActuator::compressBase() {
         return compressBaseWithRef();
     } else {
         if (!isGen2) {
-            fprintf(stderr ,"This block(%d) is Gen3 FASTQ, which currently does not support high-factor compression for this file type. Compression will be performed without a reference genome.", inBlockPtr->getBlockId());
+            fprintf(stderr ,"This block(%d) is Gen3 FASTQ, which currently does not support high-factor compression for this file type. Compression will be performed without a reference genome.\n", inBlockPtr->getBlockId());
         }
         return compressBaseWithoutRef();
     }
